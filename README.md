f6c8f1770b3ea19fd1d1deb63bdac1334bf2a465
jinaqiqi <m15711388274@163.com>
2022-03-07 10:29:00 +0800

    add

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 第二个参数为规则文件所放的位置和名称
 */
 func main() {
+    edgeDataComputing.InitMiddle()
     input := &edgeDataComputing.Input{
         Ts:         123456,
         Properties: &edgeDataComputing.Properties{
@@ -23,4 +24,4 @@ func main() {
 name:节点名称（key）
 rule:节点的规则
 */
-[{"name":"AddressNames1","rule":"AddressNames1 = AddressNames1 / 10"},{"name":"AddressNames2","rule":""},{"name":"AddressNames3","rule":"AddressNames3 = AddressNames1 / AddressNames2"}]
+[{"name":"MegnetStatus","rule":""},{"name":"Num","rule":"if MegnetStatus_last == false \u0026\u0026 MegnetStatus == true {\n\t Num = Num_last + 1\n} else {\n\t Num = Num_last\n}"},{"name":"Status","rule":"if Ia == 0 {\n\t Status = 0\n} else if Ia \u003e 40 {\n\t Status = 2\n} else {\n\t Status = 1\n}\n"},{"name":"Ia","rule":""},{"name":"Ep","rule":""},{"name":"InstantEp","rule":" InstantEp = Ep - Ep_last"}]
--- computing.go
+++ computing.go
@@ -1,4 +1,4 @@
-package edgeDataComputing
+package config
 
 import (
 	"encoding/json"
@@ -22,19 +22,53 @@ type Input struct {
 }
 
 type Properties struct {
-	AddressNames1 float64 `json:"addressNames1"`
-	AddressNames2 float64 `json:"addressNames2"`
-	AddressNames3 float64 `json:"addressNames3"`
+	MegnetStatus bool `json:"megnet_status"`
+	Num          int  `json:"num"`
+	Status       int  `json:"status"`
+	Ia           int  `json:"ia"`
+	Ep           int  `json:"ep"`
+	InstantEp    int  `json:"instant_ep"`
+}
+
+type MiddleData struct {
+	Ts         int64             `json:"ts"`
+	Properties *MiddleProperties `json:"properties"`
+}
+
+type MiddleProperties struct {
+	MegnetStatus bool `json:"megnet_status"`
+	Num          int  `json:"num"`
+	Status       int  `json:"status"`
+	Ia           int  `json:"ia"`
+	Ep           int  `json:"ep"`
+	InstantEp    int  `json:"instant_ep"`
+}
+
+var middle *MiddleData
+
+func InitMiddle() {
+	middle = &MiddleData{
+		Properties: &MiddleProperties{
+			MegnetStatus: false,
+			Num:          0,
+			Status:       0,
+			Ia:           0,
+			Ep:           0,
+			InstantEp:    0,
+		},
+	}
 }
 
 func Computing(buf []byte, path string) ([]byte, error) {
 	input := GetData(buf)
 	rule, err := GetRule(path)
+	//fmt.Println(rule, "------", err)
 	if err != nil {
 		return nil, err
 	}
 	dataContext := context.NewDataContext()
 	dataContext.Add("input", input)
+	dataContext.Add("middle", middle)
 
 	ruleBuilder := builder.NewRuleBuilder(dataContext)
 	e1 := ruleBuilder.BuildRuleFromString(rule)
@@ -46,7 +80,37 @@ func Computing(buf []byte, path string) ([]byte, error) {
 	if e != nil {
 		panic(e)
 	}
+	pushMiddleData(input)
+
 	return json.Marshal(input)
+}
+
+func pushMiddleData(input *Input) {
+	if middle.Properties.Num != input.Properties.Num {
+		middle = &MiddleData{
+			Ts: input.Ts,
+			Properties: &MiddleProperties{
+				MegnetStatus: input.Properties.MegnetStatus,
+				Num:          middle.Properties.Num + 1,
+				Status:       input.Properties.Status,
+				Ia:           input.Properties.Ia,
+				Ep:           input.Properties.Ep,
+				InstantEp:    input.Properties.InstantEp,
+			},
+		}
+	}else {
+		middle = &MiddleData{
+			Ts: input.Ts,
+			Properties: &MiddleProperties{
+				MegnetStatus: input.Properties.MegnetStatus,
+				Num:          middle.Properties.Num,
+				Status:       input.Properties.Status,
+				Ia:           input.Properties.Ia,
+				Ep:           input.Properties.Ep,
+				InstantEp:    input.Properties.InstantEp,
+			},
+		}
+	}
 }
 
 func GetRule(path string) (string, error) {
@@ -54,7 +118,7 @@ func GetRule(path string) (string, error) {
 rule "test" "test"
 begin
 `
-	ruleD := make([]ruleData, 3)
+	ruleD := make([]ruleData, 5)
 	f, err := ioutil.ReadFile(path)
 	if err != nil {
 		return "", err
@@ -88,8 +152,15 @@ func GetData(buf []byte) *Input {
 
 func ruleTmpComputer(rule string, names []string) string {
 	for _, name := range names {
-		nameTmp := "[" + name + "]" + "{" + strconv.Itoa(len(name)) + ",}"
-		replace := "input.Properties." + name
+		nameTmp2 := "[" + name + "_last]" + "{" + strconv.Itoa(len(name)+5) + ",}"
+		replace2 := " middle.Properties." + name
+		regIf2 := regexp.MustCompile(nameTmp2)
+		if regIf2 != nil {
+			rule = regIf2.ReplaceAllString(rule, replace2)
+		}
+
+		nameTmp := "[ ]+[" + name + "]" + "{" + strconv.Itoa(len(name)) + ",}"
+		replace := " input.Properties." + name
 		regIf := regexp.MustCompile(nameTmp)
 		if regIf != nil {
 			rule = regIf.ReplaceAllString(rule, replace)
